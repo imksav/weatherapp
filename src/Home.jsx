338777440622fb629951eeaebc0f1f4f5cc2bffc
Keshav Bhandari <64829597+imksav@users.noreply.github.com>
2025-01-22 05:27:30 +0000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
 
   useEffect(() => {
     axios
-      .get(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=London`)
+      .get(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=London`)
       .then((response) => {
         console.log(response);
         setData(response.data);
